2c1881f246fca95c436e6a0f8914a3dc362b30fc
Nigel Saurino <nigelsaurino@gmail.com>
2024-04-01 18:13:09 -0400

    now to create the orderbook class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <list>
 #include <vector>
+#include <memory>
 
 enum class OrderType{
     GoodTillCancel,
@@ -24,10 +25,29 @@ struct LevelInfo{
 
 typedef std::vector<LevelInfo> LevelInfos;
 
-struct Trade{
+struct TradeInfo{
     OrderId order_id_;
     Quantity quantity_;
     Price price_;
+};
+
+class Trade{
+public:
+    Trade(const TradeInfo& bid_trade, const TradeInfo& ask_trade): bid_trade_(bid_trade), ask_trade_(ask_trade){
+
+    }
+
+    TradeInfo GetBidTrade() const{
+        return bid_trade_;
+    }
+
+    TradeInfo GetAskTrade() const{
+        return ask_trade_;
+    }
+
+private:
+    TradeInfo bid_trade_;
+    TradeInfo ask_trade_;
 };
 
 typedef std::vector<Trade> Trades;
@@ -49,6 +69,84 @@ public:
 private:
     LevelInfos bids_;
     LevelInfos asks_;
+};
+
+class Order{
+public:
+    Order(OrderType order_type, OrderId order_id, Side side, Price price, Quantity quantity):
+        order_type_(order_type), order_id_(order_id), side_(side), price_(price), 
+        initial_quantity_(quantity), remaining_quantity_(quantity){
+
+        }
+
+    OrderType GetOrderType() const{
+        return order_type_;
+    }
+
+    OrderId GetOrderId() const{
+        return order_id_;
+    }    
+
+    Side GetSide() const{
+        return side_;
+    }
+
+    Price GetPrice() const{
+        return price_;
+    }
+
+    Quantity GetInitialQuantity() const{
+        return initial_quantity_;
+    }
+
+    Quantity GetRemainingQuantity() const{
+        return remaining_quantity_;
+    }
+
+    Quantity GetFilled() const{
+        return GetInitialQuantity() - GetRemainingQuantity();
+    }
+
+    bool IsFilled() const{
+        return GetRemainingQuantity() == 0;
+    }
+
+    void Fill(Quantity quantity){
+        if(quantity > remaining_quantity_){
+            throw std::logic_error("Attempted to fill a quantity of " + std::to_string(quantity) +
+                               ", which exceeds the remaining quantity of " + std::to_string(remaining_quantity_) +
+                               " in the current order.");
+        }
+
+        remaining_quantity_ -= quantity;
+    }
+
+
+private:
+    OrderType order_type_;
+    OrderId order_id_;
+    Side side_;
+    Price price_;
+    Quantity initial_quantity_;
+    Quantity remaining_quantity_;
+
+};
+
+typedef std::shared_ptr<Order> OrderPointer;
+typedef std::list<OrderPointer> OrderPointers;
+
+class OrderModify{
+public:
+    OrderModify(OrderId order_id, Side side, Price price, Quantity quantity): 
+                order_id_(order_id), side_(side), price_(price), quantity_(quantity){
+
+                } 
+
+private:
+    OrderId order_id_;
+    Side side_;
+    Price price_;
+    Quantity quantity_;
 };
 
 int main(int, char**){
